af9bc61b5b3b942bcbeb2a5e03947c4b75f6d68e
Bogdan <142217589+beautymental@users.noreply.github.com>
2025-12-24 08:52:27 +0200

    Update SettingsForm.jsx

--- lab3/src/widgets/SettingsForm.jsx
+++ lab3/src/widgets/SettingsForm.jsx
@@ -34,7 +34,6 @@ export default function SettingsForm() {
     defaultValues: settings,
   });
 
-  // якщо settings оновились ззовні — синхронізуємо форму
   useEffect(() => {
     reset(settings);
   }, [settings, reset]);
